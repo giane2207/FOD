9fc052db2ea4e28f26fdd65f7a89d0b4f91d7b99
Gianella Graneros <gianella2209@gmail.com>
2025-03-21 16:05:32 -0300

    Nuevo commit

--- Practica Resuelta/ejercicio1.pas
+++ Practica Resuelta/ejercicio1.pas
@@ -1,1 +1,8 @@
-
+program ejercicio1;
+var 
+     suma: Integer;
+begin
+     suma = 5;
+     suma = suma + 10;
+     WriteLn(suma);
+end.
---
+++ Practica Resuelta/probando.pas
@@ -0,0 +1,4 @@
+program probando;
+begin
+  WriteLn('Hola mundooo!!!!!');
+end.
